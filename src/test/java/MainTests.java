1fd97e170b9e4c40c6925dd5e152bb3e97dcbcd7
Abramichev <endi79@mail.ru>
2023-03-19 14:45:24 +0300

    change project commit

---
+++ src/test/java/MainTests.java
@@ -0,0 +1,132 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import yandex.stellarburgers.api.BaseClient;
+import yandex.stellarburgers.model.Client;
+import yandex.stellarburgers.pageobjects.AuthPage;
+import yandex.stellarburgers.pageobjects.MainPage;
+import yandex.stellarburgers.pageobjects.RegistrationPage;
+
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.page;
+
+
+public class MainTests extends BaseClient {
+
+    private RegistrationPage registrationPage;
+    private AuthPage authPage;
+    private MainPage mainPage;
+    private Client registrationCorrectData;
+
+    @Step("Initialization of test data")
+    @Before
+    public void createData(){
+        registrationPage = page(RegistrationPage.class);
+        authPage = page(AuthPage.class);
+        mainPage = page(MainPage.class);
+        registrationCorrectData = new Client(RandomStringUtils.randomAlphabetic(10) + "@yandex.ru", RandomStringUtils.randomAlphabetic(10), RandomStringUtils.randomAlphanumeric(10));
+        BaseClient.createClient(registrationCorrectData);
+    }
+
+    @Step("Deleting test data after tests")
+    @After
+    public void deleteClient(){
+        BaseClient.deleteClient(registrationCorrectData);
+        close();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Click personal account button")
+    @Description("Should redirect to personal account")
+    public void shouldRedirectToPersonalAccount(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickPersonalAccountButton();
+        authPage.setEmailInputAlreadyRegisteredUser(registrationCorrectData.getEmail());
+        authPage.setPasswordInputAlreadyRegisteredUser(registrationCorrectData.getPassword());
+        authPage.clickSignInButton();
+        mainPage.clickPersonalAccountButton();
+        mainPage.checkPersonalAccount();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Constructor from personal account")
+    @Description("Should be able to click Constructor-button from personal account")
+    public void shouldClickToConstructorFromPersonalAccount(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickPersonalAccountButton();
+        authPage.setEmailInputAlreadyRegisteredUser(registrationCorrectData.getEmail());
+        authPage.setPasswordInputAlreadyRegisteredUser(registrationCorrectData.getPassword());
+        authPage.clickSignInButton();
+        mainPage.clickPersonalAccountButton();
+        mainPage.clickConstructorButton();
+        mainPage.checkMainPage();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Logo from personal account")
+    @Description("Should be able to click Logo-button from personal account")
+    public void shouldClickToLogoFromPersonalAccount(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickPersonalAccountButton();
+        authPage.setEmailInputAlreadyRegisteredUser(registrationCorrectData.getEmail());
+        authPage.setPasswordInputAlreadyRegisteredUser(registrationCorrectData.getPassword());
+        authPage.clickSignInButton();
+        mainPage.clickPersonalAccountButton();
+        mainPage.clickLogo();
+        mainPage.checkMainPage();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Check logout button")
+    @Description("Should be able to logout from personal account")
+    public void shouldLogOutFromAccount(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickPersonalAccountButton();
+        authPage.setEmailInputAlreadyRegisteredUser(registrationCorrectData.getEmail());
+        authPage.setPasswordInputAlreadyRegisteredUser(registrationCorrectData.getPassword());
+        authPage.clickSignInButton();
+        mainPage.clickPersonalAccountButton();
+        mainPage.clickLogoutButton();
+        mainPage.checkLogout();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Check Bun button")
+    @Description("Should scroll to bun-menu")
+    public void shouldBeBunMenu(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickSauceButton();
+        mainPage.clickBunButton();
+        mainPage.checkBun();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Check Sauce button")
+    @Description("Should scroll to Sauce-menu")
+    public void shouldBeSauceMenu(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickSauceButton();
+        mainPage.checkSauce();
+    }
+
+    @Step("Checking the home page")
+    @Test
+    @DisplayName("Check Filling button")
+    @Description("Should scroll to Filling-menu")
+    public void shouldBeFillingMenu(){
+        mainPage.openPage(BaseURL);
+        mainPage.clickFillingButton();
+        mainPage.checkFilling();
+    }
+}
---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,63 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import yandex.stellarburgers.api.BaseClient;
+import yandex.stellarburgers.model.Client;
+import yandex.stellarburgers.pageobjects.RegistrationPage;
+
+import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.close;
+
+public class RegistrationTest extends BaseClient {
+
+    private RegistrationPage registrationPage;
+    private Client registrationCorrectData;
+    private Client registrationInCorrectData;
+
+    @Step("Initialization of test data")
+    @Before
+    public void createData(){
+        registrationPage = page(RegistrationPage.class);
+        registrationCorrectData = new Client(RandomStringUtils.randomAlphabetic(10) + "@yandex.ru", RandomStringUtils.randomAlphabetic(10), RandomStringUtils.randomAlphanumeric(10));
+        registrationInCorrectData = new Client(RandomStringUtils.randomAlphabetic(10) + "@yandex.ru", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphanumeric(5));
+    }
+
+    @Step("Deleting test data after tests")
+    @After
+    public void deleteClient(){
+        BaseClient.deleteClient(registrationCorrectData);
+        close();
+    }
+
+    @Step("Checking Registration")
+    @Test
+    @DisplayName("Registration new user with correct password")
+    @Description("Should create new user")
+    public void shouldCreateNewUserWithCorrectPassword() {
+        registrationPage.openPage(registrationURL);
+        registrationPage.setNameInput(registrationCorrectData.getName());
+        registrationPage.setEmailInput(registrationCorrectData.getEmail());
+        registrationPage.setPasswordInput(registrationCorrectData.getPassword());
+        registrationPage.clickRegisterButton();
+        registrationPage.checkRegistration();
+    }
+
+    @Step("Checking Registration")
+    @Test
+    @DisplayName("Registration new user with incorrect password (less 6 symbols)")
+    @Description("Should be error with short password")
+    public void shouldBeErrorWithBadPassword() {
+        registrationPage.openPage(registrationURL);
+        registrationPage.setNameInput(registrationInCorrectData.getName());
+        registrationPage.setEmailInput(registrationInCorrectData.getEmail());
+        registrationPage.setPasswordInput(registrationInCorrectData.getPassword());
+        registrationPage.clickRegisterButton();
+        registrationPage.checkPassword();
+    }
+
+}
